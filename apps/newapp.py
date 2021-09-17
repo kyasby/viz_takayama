9c6827a61eccf7394b52bdf7f0f701fded08a582
kyasby <kryo.iryo.tryo@gmail.com>
2021-09-17 23:21:27 +0900

    make neat

--- apps/newapp.py
+++ apps/newapp.py
@@ -13,170 +13,183 @@ from statsmodels.tsa.seasonal import STL
 OBJ_TYPE = ["person", "car", "bus", "truck", "bicycle", "motorbike"]
 
 
+def set_params():
+  # set sidebar
+  global obj_type_selector, date_start, date_end, place
 
-def app():
+  place = st.sidebar.selectbox("場所を選んでください。", ["マルプ", "16"])
 
-
-  def set_params():
-    # set sidebar
-    global obj_type_selector, date_start, date_end, place
-    place = st.sidebar.selectbox("場所を選んでください。", ["マルプ", "16"])
-    obj_type_selector = st.sidebar.selectbox("Select your favorite flower", OBJ_TYPE)
-
-    # col1, col2 = st.sidebar.columns(2)#\n May：　5月\n June：　6月
-    # col1.markdown("January：　1月\n Feburary:　2月\n March：　3月\n April：　4月\n May　：　5月\n June：　6月")
-    # col2.markdown("July：　7月\n August：　8月\n September：　9月\n October：　10月\n November：　11月\n December：　12月")
+  obj_type_selector = st.sidebar.selectbox("Select your favorite flower", OBJ_TYPE)
 
 
-    date_start = st.sidebar.date_input('開始日',
-                                 min_value=date(2021,6,26),
-                                 max_value=date(2021,9,3),
-                                 value=date(2021, 7, 1),
-                                 )
+  date_start = st.sidebar.date_input('開始日',
+                               min_value=date(2021,6,26),
+                               max_value=date(2021,9,3),
+                               value=date(2021, 7, 1),
+  )
 
-    date_end   = st.sidebar.date_input('終了日',
-                                 min_value=date(2021,6,26),
-                                 max_value=date(2021,9,3),
-                                 value=date(2021, 9, 3),
-                                 )
+  date_end   = st.sidebar.date_input('終了日',
+                               min_value=date(2021,6,26),
+                               max_value=date(2021,9,3),
+                               value=date(2021, 9, 3),
+  )
 
-    df = pd.DataFrame(
-            {"English": ["January", "Feburary","March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-             "日本語": ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]}
-     )
+  df = pd.DataFrame(
+          {"English": ["January", "Feburary","March", "April",
+                       "May", "June", "July", "August", "September", "October", "November", "December"],
+           "日本語": ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]}
+   )
 
-    st.sidebar.table(df) 
-
-
-    params={"obj_type_selector": obj_type_selector,
-            "date_start": date_start,
-            "date_end": date_end,
-            "place": place
-
-    }
-    return params
+  st.sidebar.table(df) 
 
 
-  @st.cache()
-  def load_date(parama):
+  params={"obj_type_selector": obj_type_selector,
+          "date_start": date_start,
+          "date_end": date_end,
+          "place": place
 
-    # 後から綺麗にする
-    if params["place"] == "マルプ":
-      url = "https://publickbacketmdg.s3.ap-northeast-1.amazonaws.com/day_grouped_by_%E3%81%BE%E3%82%8B%E3%81%A3%E3%81%A8%E3%83%95%E3%82%9A%E3%83%A9%E3%82%B5%E3%82%99.csv"
-      area = "plaza_car_near"
-    else:
-      url = "https://publickbacketmdg.s3.ap-northeast-1.amazonaws.com/day_grouped_by_%E6%9C%9B%E9%81%A0.csv"
-      area = "juroku_zoom"
+  }
+  return params
 
 
-    df = pd.read_csv(url)
-    df.day = pd.to_datetime(df.day)
-    df = df[df["area"]==area]
+# 後から綺麗にする
+@st.cache()
+def load_date(params):
 
-    df = df[df["name"] == params["obj_type_selector"]]
-
-    if params["place"]=="マルプ":
-      df = df[(pd.to_datetime(params["date_start"]) < df.day) & (df.day  < pd.to_datetime(params["date_end"]))]
-    else:
-      df = df[(pd.to_datetime(params["date_start"]) < pd.to_datetime(df.date)) & (pd.to_datetime(df.date)  < pd.to_datetime(params["date_end"]))]
-
-    return url, df
+  if params["place"] == "マルプ":
+    url = "https://publickbacketmdg.s3.ap-northeast-1.amazonaws.com/day_grouped_by_%E3%81%BE%E3%82%8B%E3%81%A3%E3%81%A8%E3%83%95%E3%82%9A%E3%83%A9%E3%82%B5%E3%82%99.csv"
+    area = "plaza_car_near"
+  else:
+    url = "https://publickbacketmdg.s3.ap-northeast-1.amazonaws.com/day_grouped_by_%E6%9C%9B%E9%81%A0.csv"
+    area = "juroku_zoom"
 
 
-  def draw_data(df, params):
+  df = pd.read_csv(url)
+  df.day = pd.to_datetime(df.day)
+  df = df[df["area"]==area]
 
-    fig = px.line(df, x='day', y="timestamp", color="countingDirection")
+  # 種類を絞る
+  df = df[df["name"] == params["obj_type_selector"]]
 
-    # add_data
-    df_add = df[df["countingDirection"]=="leftright_topbottom"]
-    # fig.add_trace(go.Scatter(x=df_add["day"], y=df_add["timestamp"], name='your data'))
+  if params["place"]=="マルプ":
+    df = df[(pd.to_datetime(params["date_start"]) < df.day) & (df.day  < pd.to_datetime(params["date_end"]))]
+  else:
+    df = df[(pd.to_datetime(params["date_start"]) < pd.to_datetime(df.date)) & (pd.to_datetime(df.date)  < pd.to_datetime(params["date_end"]))]
 
-    fig.update_layout(xaxis_title="日付",
-                      yaxis_title="数",
-                      xaxis_tickformat = '%Y-%m-%d',)
-    fig.add_vrect(
-        x0="2021-08-01", x1="2021-08-06",
-        fillcolor="LightSalmon", opacity=0.5,
-        layer="below", line_width=0,
-    )
-    st.plotly_chart(fig, use_container_width=True)
+  return url, df
 
+#選択場所のグラフを描く
+def draw_data(df, params):
 
-  def draw_trend(original):
+  fig = px.line(df, x='day', y="timestamp", color="countingDirection")
 
-    def upload_csv():
-      global count_col
-      global day_col
-      
-      csv = st.file_uploader("ファイルアップロード", type='csv')
-      if csv:
-        df = pd.read_csv(csv)
-        st.markdown("アップロードファイルのプレビュー")
-        st.dataframe(df.head(3))
-        count_col = st.text_input("比較する値のカラム名を入れてください。", value="timestamp") # todo default
-        day_col   = st.text_input("日付のカラム名を入れてください。", value="date") # todo default
-        if count_col and day_col:
+  # add_data
+  df_add = df[df["countingDirection"]=="leftright_topbottom"]
+  # fig.add_trace(go.Scatter(x=df_add["day"], y=df_add["timestamp"], name='your data'))
+
+  fig.update_layout(xaxis_title="日付",
+                    yaxis_title="数",
+                    xaxis_tickformat = '%Y-%m-%d',)
+  fig.add_vrect(
+      x0="2021-08-01", x1="2021-08-06",
+      fillcolor="LightSalmon", opacity=0.5,
+      layer="below", line_width=0,
+  )
+  st.plotly_chart(fig, use_container_width=True)
+
+# トレンドのラインを描く
+def draw_trend(place_name, place_df):
+
+  def upload_csv():
+    global count_col
+    global day_col
+    
+    csv = st.file_uploader("ファイルアップロード", type='csv')
+    if csv:
+      df = pd.read_csv(csv)
+
+      # プレビュ
+      st.markdown("アップロードファイルのプレビュー")
+      st.dataframe(df.head(3))
+
+      #　カラム入力
+      count_col = st.text_input("比較する値のカラム名を入れてください。", value="timestamp") # todo default
+      day_col   = st.text_input("日付のカラム名を入れてください。", value="date") # todo default
+      if count_col and day_col:
+        try:
           df["day"] = pd.to_datetime(df[day_col])
+        except KeyError as e:
+          st.error("日付のカラムを正しく入力してください。")
+        else:
           return df
 
 
-    @st.cache()
-    def make_comparing_df(original, uploaded):
+  @st.cache(suppress_st_warning=True)
+  def combine_df(place_df, uploaded):
 
-      def convert_df(df, day_col="day", dim="D"):
-        df = df.set_index("day")
-        df = df.resample(dim).sum()
-        return df 
+    def convert_df(df, day_col="day", dim="D"):
+      df = df.set_index("day")
+      df = df.resample(dim).sum()
+      return df 
 
-      def extract_trend(df, col, period=7):
+    def extract_trend(df, col, period=7):
+      try:
         stl=STL(df[col], period=period, robust=True)
+      except KeyError as e:
+        st.error('比較するカラム名を正しく入れてください!')
+      else:
         stl_series = stl.fit()
-        
-        return pd.DataFrame(stl_series.trend) 
-
-      original = convert_df(original)
-      original = extract_trend(original, "timestamp")
       
-      uploaded = convert_df(uploaded)
-      uploaded = extract_trend(uploaded, count_col)
+      return pd.DataFrame(stl_series.trend) 
 
-      original["trend"] /= original["trend"].max()
-      uploaded["trend"] /= uploaded["trend"].max()
+    place_df = convert_df(place_df)
+    place_df = extract_trend(place_df, "timestamp")
+    
+    uploaded = convert_df(uploaded)
+    uploaded = extract_trend(uploaded, count_col)
 
-      original["type"] = "original"
-      uploaded["type"] = "uploaded"
+    # 最大値との比にする
+    place_df["trend"] /= place_df["trend"].max()
+    uploaded["trend"] /= uploaded["trend"].max()
 
-      df = pd.concat([original, uploaded], axis=0)
+    place_df["type"] = place_name
+    uploaded["type"] = "uploaded"
 
-      return df
+    df = pd.concat([place_df, uploaded], axis=0)
 
+    return df
 
-    with st.expander("自分のデータと比べる"):
-      st.markdown('日付と数値を含む`csv`をアップロードすると，通行人のトレンドと比較することができます。\
-                  \n 日付：`yyyy-mm-dd`の形式にしてください。例：2021-01-29\
-                  \n 数値：数字のみ入れてください。少数も可能です。')
-      
-      show_csv_sample = st.radio("アップロードするcsvのサンプル",
-                                ('表示', '非表示'), index=1)
-      if show_csv_sample == "表示":
-        st.dataframe(original.head(3))
+  with st.expander("自分のデータをアップロードして，この地点のデータと比べる"):
+    st.markdown('日付と数値を含む`csv`をアップロードすると，通行人のトレンドと比較することができます。\
+                例えば，通行人の人数が増えている際に，売り上げも増えているか比較することができます。'
+    )
 
-      uploaded = upload_csv()
-      if uploaded is not None:
-        df = make_comparing_df(original, uploaded)
+    st.markdown('日付：`yyyy-mm-dd`の形式にしてください。例：`2021-01-29`\
+                \n 数値：数字のみ入れてください。少数も可能です。'
+    )
+    
+    show_csv_sample = st.radio("アップロードするcsvのサンプル", ('表示', '非表示'), index=1)
+    if show_csv_sample == "表示":
+      st.dataframe(place_df.head(3))
 
-        fig = px.line(df, y="trend", color="type")
-        fig.update_layout(xaxis_title="日付",
-                          yaxis_title="増減",
-                          xaxis_tickformat = '%Y-%m-%d',)
-        st.plotly_chart(fig, use_containupler_width=True)
+    uploaded_df = upload_csv()
+    if uploaded_df is not None:
+      df = combine_df(place_df, uploaded_df)
 
+      fig = px.line(df, y="trend", color="type", title="大まかな変化の比較")
+      fig.update_layout(xaxis_title="日付",
+                        yaxis_title="増減",
+                        xaxis_tickformat = '%Y-%m-%d',)
+      st.plotly_chart(fig, use_containupler_width=True)
 
-
+def app():
 
   params = set_params()
   url, df = load_date(params)
-  st.title(urllib.parse.unquote(f'{url.split("/")[-1].split(".")[0].split("_")[-1]}'))
+
+  place_name = urllib.parse.unquote(f'{url.split("/")[-1].split(".")[0].split("_")[-1]}')
+  st.title(place_name)
+  
   draw_data(df, params)
-  draw_trend(df)
+  draw_trend(place_name, df)
 
