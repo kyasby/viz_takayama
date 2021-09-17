524eb31689a5848dfd3a3d13c36385fe6bf6bb95
kyasby <kryo.iryo.tryo@gmail.com>
2021-09-17 10:14:01 +0900

    change csv url to pablick

--- apps/newapp.py
+++ apps/newapp.py
@@ -11,7 +11,7 @@ OBJ_TYPE = ["person", "car", "bus", "truck", "bicycle", "motorbike"]
 
 def app():
   # load data
-  URL = "/Users/ryo/Documents/Lab/Jetson/jetson_csv/9-3/day_grouped_by_まるっとプラザ.csv"
+  URL = 'https://jetson-extraction.s3.ap-northeast-1.amazonaws.com//day_grouped_by_%E3%81%BE%E3%82%8B%E3%81%A3%E3%81%A8%E3%83%95%E3%82%9A%E3%83%A9%E3%82%B5%E3%82%99.csv'
   DF = pd.read_csv(URL)
   DF = DF[DF["area"]=="plaza_car_near"]
   DF.day = pd.to_datetime(DF.day)
